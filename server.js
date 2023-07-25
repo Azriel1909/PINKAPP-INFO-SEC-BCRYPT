e09ac169f460619c39170264e416bb6237470ce5
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-07-25 17:45:09 -0600

    Hash compare passwords

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ app.get('/', (req, res) => {
   res.send('Server live!');
 });
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res)
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
-
 
 //END_SYNC
 
